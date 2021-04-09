9d11d9d852a3dc08cd0be0ab8a16216a2e44627d
maamari <maamari@usc.edu>
2021-04-09 16:04:21 -0500

    add new code

---
+++ dataDist/bridge2.py
@@ -0,0 +1,55 @@
+import socket
+from _thread import *
+
+sock = socket.socket()
+bridge_sock = socket.socket()
+SERV_IP = '127.0.0.1'
+SERV_PORT = 2222
+BRIDGE_IP = '127.0.0.1'
+BRIDGE_PORT = 2002
+BUFFER_SIZE = 2048
+clients = []
+
+# Connect bridge to server
+print('Connecting to server...')
+try:
+    sock.connect((SERV_IP, SERV_PORT))
+except socket.error as e:
+    print(str(e))
+
+# Start up bridge
+try:
+    bridge_sock.bind((BRIDGE_IP, BRIDGE_PORT))
+except socket.error as e:
+    print(str(e))
+
+# Listen for client
+print('Waiting for client...')
+bridge_sock.listen(5)
+
+# Accept new client on thread
+def new_client(client):
+    while True:
+        '''
+        NEED TO FIX:
+        There's a bug in here, for the first pass of data sent to a client, multiple values are combined and sent at once. 
+        '''
+        #if i==0: continue 
+        data = sock.recv(BUFFER_SIZE)    
+        print(data.decode('utf-8'))    
+        for c in clients:
+            try: c.sendall(data)            
+            except socket.error as e:
+                print(str(e))
+                clients.remove(c)
+    client.close()
+    sock.close()
+
+# Accept/place clients on threads
+while True:
+    cli, addr = bridge_sock.accept()
+    clients.append(cli)
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+    
+    start_new_thread(new_client, (cli, ))
+bridge_sock.close()
---
+++ dataDist/server2.py
@@ -0,0 +1,43 @@
+import socket
+import os
+from _thread import *
+import random 
+import time
+
+sock = socket.socket()
+TCP_IP = '127.0.0.1'
+TCP_PORT = 2222
+BUFFER_SIZE = 2048
+clients = []
+
+# Start server
+try:
+    sock.bind((TCP_IP, TCP_PORT))
+except socket.error as e:
+    print(str(e))
+
+# Listen for client
+print('Waiting for bridge...')
+sock.listen(5)
+
+# Accept new client on thread
+def new_client(client):
+    while True:
+        #data = client.recv(BUFFER_SIZE)
+        time.sleep(1)
+        data = random.randint(0,9)
+        for c in clients:
+            c.sendall(str(data).encode('utf-8'))
+    client.close()
+
+# Accept/place clients on threads
+while True:
+    cli, addr = sock.accept()
+    clients.append(cli)
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+#    for c in clients:
+#        time.sleep(2)
+#        data = random.randint(0,9)**2        
+#        c.sendall(str(data).encode('utf-8'))
+    start_new_thread(new_client, (cli, ))
+sock.close()
