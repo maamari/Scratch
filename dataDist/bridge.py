e972b872b48d72ca661bfa0cb82f939ff403d95d
maamari <maamari@usc.edu>
2021-04-22 00:17:56 -0500

    update single thread code

--- dataDist/bridge.py
+++ dataDist/bridge.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
 import threading
+import queue
 
 sock = socket.socket()
 bridge_sock = socket.socket()
@@ -9,7 +10,7 @@ SERV_PORT = 2222
 BRIDGE_IP = '127.0.0.1'
 BRIDGE_PORT = 2002
 BUFFER_SIZE = 2048
-clients = []
+clients = queue.Queue()
 
 # Connect bridge to server
 print('Connecting to server...')
@@ -29,29 +30,33 @@ print('Waiting for client...')
 bridge_sock.listen(5)
 
 # Accept new client on threads
-def new_client(client):
+def new_client(clis):
+    
     while True:
         data = sock.recv(BUFFER_SIZE)    
         print(data.decode('utf-8'))    
-        for c in clients:
+        
+        for c in list(clients.queue):
             try: c.sendall(data)            
             except socket.error as e:
                 print(str(e))
                 clients.remove(c)
-    client.close()
+    
+    #client.close()
     sock.close()
 
 # Accept/place clients on threads
+threading.Thread(target = new_client, args = (clients, )).start()
 while True:
     cli, addr = bridge_sock.accept()
-    clients.append(cli)
+    clients.put(cli)
     print('Connected to: ' + addr[0] + ':' + str(addr[1]))
     print(clients)
-
+    
+    for thread in threading.enumerate(): 
+        print(thread.name)
+    
     #new_client(cli)
     #start_new_thread(new_client, (cli, ))
-    for c in clients:
-        thread1 = threading.Thread(target = new_client, args = (c, ))
-    thread1.start()
 
 bridge_sock.close()
