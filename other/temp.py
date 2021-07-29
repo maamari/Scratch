8074bf860a8df22f14e69a208c0fb25852d80fae
maamari <maamari@usc.edu>
2021-07-29 10:22:27 -0500

    add working

--- other/temp.py
+++ other/temp.py
@@ -1,28 +1,43 @@
 from pymap3d import aer2ecef
 from math import degrees,atan2, sqrt, sin, cos, radians, pi
 import numpy as np
+import matplotlib.pyplot as plt
 
 R = 6378137
 R2 = 20350000
 flat = 298.257223563
 
+a = 6378137.0 #!
+b = 6356752.314245 #!
+a2 = a*a #!
+b2 = b*b #!
+e2 = 1-(b2/a2) #!
+e = e2/(1-e2) #!
+
+def aer2enu(azi,ele,rang):
+    z = rang*sin(radians(ele))
+    r = rang*cos(radians(ele))
+    x = r*sin(radians(azi))
+    y = r*cos(radians(azi))
+    return x,y,z
+
 def ecef2geo3(x, y, z):
-    a = 6378137.0
-    b = 6356752.314245
-    a2 = a*a
-    b2 = b*b
-    e2 = 1-(b2/a2)
-    e = e2/(1-e2)
-    p = np.sqrt(x*x + y*y)
-    q = np.arctan2(a * z, b * p)
+    a = 6378137.0 #!
+    b = 6356752.314245 #!
+    a2 = a*a #!
+    b2 = b*b #!
+    e2 = 1-(b2/a2) #!
+    e = e2/(1-e2) #!
+    p = np.sqrt(x*x + y*y) #!
+    q = np.arctan2(a * z, b * p) #!
     
-    sq = np.sin(q)
-    cq = np.cos(q)
+    sq = np.sin(q)  #!
+    cq = np.cos(q)  #!
     
-    sq3 = sq * sq * sq
-    cq3 = cq * cq * cq
+    sq3 = sq * sq * sq #!
+    cq3 = cq * cq * cq #!
     
-    phi = np.arctan2(z + e * b * sq3, p - e2 * a * cq3)
+    phi = np.arctan2(z + e * b * sq3, p - e2 * a * cq3) #!
     lmd = np.arctan2(y, x)
     v = a / np.sqrt(1.0 - e2 * np.sin(phi) * np.sin(phi))
     
@@ -30,14 +45,7 @@ def ecef2geo3(x, y, z):
     lon = np.rad2deg(lmd)
     h = (p / np.cos(phi)) - v
 
-    print(round(lat,2),round(lon,2),round(h,2))
-
-def ecef2geo(x,y,z):
-    rho = sqrt(pow(x,2)+pow(y,2)+pow(z,2))
-    lat = cyl2geo(rho,z,R,flat)
-    lon = atan2(y,x)
-   
-    print(degrees(lat[0]), degrees(lon), lat[1])
+    return lon,lat
 
 def cyl2geo(rho,z,a,f):
     b = (1-f)*a
@@ -61,35 +69,35 @@ def cyl2geo(rho,z,a,f):
 
     return phi,h
 
-def aer2geo(azi, ele):
-    sRange = sqrt(pow(R*cos(radians(ele)),2)+pow(R2,2)-pow(R,2))-R*cos(radians(ele))
-    print(sRange)
-    x,y,z = aer2ecef(198,42,sRange, 36.0926,-76.3829,0)
-    return [x,y,z]
-
 def aer2geo2(azi,ele):
     sRange = sqrt(pow(R*cos(radians(ele)),2)+pow(R2,2)-pow(R,2))-R*cos(radians(ele))
     x,y,z = aer2ecef2(azi,ele,sRange)
-    #x,y,z = aer2ecef(azi,ele,sRange)
-    ecef2geo3(x,y,z)
+    lon, lat = ecef2geo3(x,y,z)
+    return lon,lat
+
+def geo2ecef(lat,lon,alt):
+    lat = radians(lat)
+    lon = radians(lon)
+
+    N = a**2/sqrt(a**2*cos(lat)**2 + b**2*sin(lat)**2)
+
+    x = (N+alt)*cos(lat)*cos(lon)
+    y = (N+alt)*cos(lat)*sin(lon)
+    z = (N*(b/a)**2+alt)*sin(lat)
+
+    return x,y,z
 
 def aer2ecef2(azi,ele,rang):
-    x,y,z = geo2ecef(36.0926,-76.3829)
+    x,y,z = geo2ecef(36.0926,-76.3829,0.)
     x1,y1,z1 = aer2enu(azi,ele,rang)
-    dx,dy,dz = enu2ecefv(x1,y1,z1,36.0926,-76.3829,0)
-
-    x_f = x1+dx
-    y_f = y1+dy
-    z_f = z1+dz
+    dx,dy,dz = enu2ecefv(x1,y1,z1,36.0926,-76.3829,0.)
+    print(x,y,z)
+    print(dx,dy,dz)
+    x_f = x+dx
+    y_f = y+dy
+    z_f = z+dz
+    
     return x_f,y_f,z_f
-
-
-def aer2enu(azi,ele,rang):
-    z = rang*sin(radians(ele))
-    r = rang*cos(radians(ele))
-    x = r*sin(radians(azi))
-    y = r*cos(radians(azi))
-    return x,y,z
 
 def enu2ecefv(x,y,z,x1,y1,z1):
     cosphi = cos(radians(x1))
@@ -105,26 +113,15 @@ def enu2ecefv(x,y,z,x1,y1,z1):
 
     return u,v,w
 
-def geo2ecef(lon,lat):
-    rho,z = geo2cyl(lat,0,R,flat)
-    x = rho*cos(radians(lon))
-    y = rho*cos(radians(lon))
-   
-    return x,y,z
+azieles = []
+for j in range(91):
+    aziele = []
+    for i in range(365):
+        aziele.append([i,j])
+    azieles.append(aziele)
 
-def geo2cyl(phi,h,a,f):
-    sinphi = sin(radians(phi))
-    cosphi = cos(radians(phi))
-
-    e2 = f*(2-f)
-    N = a/sqrt(1-e2*pow(sinphi,2))
-    rho = (N+h)*cosphi
-    z = (N*(1-e2)+h)*sinphi
-
-    return rho,z
-
-azieles = [[360,90],
-           [267,44],
+'''
+        [267,44],
            [37,10],
            [75,6],
            [315,71],
@@ -135,12 +132,30 @@ azieles = [[360,90],
            [53,64],
            [202,20],
            [265,8]]
+
 print("lat\tlon\talt")
 print("-------------------------------------------")
-for coord in azieles:
-    aer2geo2(coord[0],coord[1])
+'''
+
+lon,lat = aer2geo2(360,90)
+print(lon,lat)
+
+'''
+for i,ae in enumerate(azieles):
+    for coord in ae:
+        lon,lat = aer2geo2(coord[0],coord[1])
+        plt.ylim(-90,90)
+        plt.xlim(-180,180)
+        plt.ylabel('Latitude')
+        plt.xlabel('Longitude')
+        plt.scatter(lon,lat,c='black',s=1)
+        #print(lon,lat, coord[0],coord[1])
+
+    if i==90:
+        plt.text(-50,0,"{},{}".format(round(lon,2),round(lat,2)))
+    plt.savefig('{}.png'.format(i))
+    plt.clf()
+'''
 
 #res = aer2geo(198,420)
 #print(res[0],res[1],res[2])
-
-
